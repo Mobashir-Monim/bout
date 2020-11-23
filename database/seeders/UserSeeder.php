533ff80538e2a07f8d4dda8bae2cc76ae8ea283e
Mobashir Monim <mobashirmonim@gmail.com>
2020-11-23 22:28:50 +0600

    Create user seeders

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -14,6 +14,12 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        User::create(['name' => 'Mobashir Monim', 'password' => bcrypt('123'), 'email' => 'm@b.c']);
+        User::create(['name' => 'Mobashir Monim', 'password' => bcrypt(''), 'email' => 'mobashir.monim@bracu.ac.bd']);
+        User::create(['name' => 'Mahbubul Alam Majumdar', 'password' => bcrypt(''), 'email' => 'majumdar@bracu.ac.bd']);
+        User::create(['name' => 'Sadia Hamid Kazi', 'password' => bcrypt(''), 'email' => 'skazi@bracu.ac.bd']);
+        User::create(['name' => 'Arif Shakil', 'password' => bcrypt(''), 'email' => 'arif.shakil@bracu.ac.bd']);
+        User::create(['name' => 'MD Tanzim Reza', 'password' => bcrypt(''), 'email' => 'tanzim.reza@bracu.ac.bd']);
+        User::create(['name' => 'Annajiat Alim Rasel', 'password' => bcrypt(''), 'email' => 'annajiat@bracu.ac.bd']);
+        User::create(['name' => 'Warida Rashid', 'password' => bcrypt(''), 'email' => 'warida.rashid@bracu.ac.bd']);
     }
 }
