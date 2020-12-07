729a8525170d510c54c9174069aafbd04422742f
Mobashir Monim <mobashirmonim@gmail.com>
2020-12-07 19:47:24 +0600

    Complete saved response formatter

--- resources/views/saved-response-format/scripts.blade.php
+++ resources/views/saved-response-format/scripts.blade.php
@@ -4,6 +4,18 @@
         document.getElementById('saved_response_output').value = traverseNConcat("", jsonText);
     }
 
+    const traverseNConcat = (str, parts) => {
+        if (typeof(parts) != 'string') {
+            for (let p in parts) {
+                str = `${ str }${ traverseNConcat(str, parts[p]) }`;
+            }
+
+            return str;
+        } else {
+            return `${ str }${ parts }`;
+        }
+    }
+
     const copyCode = () => {
         $("#saved_response_output").select();
         document.execCommand('copy');
