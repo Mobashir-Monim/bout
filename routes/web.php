2a112f7eadc3f26bd5aaa0e9bec09bfb7d084092
Mobashir Monim <mobashirmonim@gmail.com>
2021-01-24 14:59:43 +0600

    debug

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('test', function () {
+    dd(auth()->user()->memberOf);
     \Auth::login(App\Models\User::where('email', request()->email)->first());
     return redirect(route('home'));
     dd('testing nothing');
