bd88b59d01f021224233b1d239d3d7ecbf6adf1c
Mobashir Monim <mobashirmonim@gmail.com>
2021-02-09 03:39:24 +0600

    Update configs table

--- database/migrations/2020_10_29_173425_create_configs_table.php
+++ database/migrations/2020_10_29_173425_create_configs_table.php
@@ -15,7 +15,7 @@ class CreateConfigsTable extends Migration
     {
         Schema::create('configs', function (Blueprint $table) {
             $table->string('id')->primary();
-            $table->mediumText('value');
+            $table->json('configs');
             $table->timestamps();
         });
     }
