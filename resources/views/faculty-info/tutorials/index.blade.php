8b9c39be5d47167b06641710e59b8f1364d9ad3a
Mobashir Monim <mobashirmonim@gmail.com>
2021-03-10 22:54:19 +0600

    Complete services and update card left image css

--- resources/views/faculty-info/tutorials/index.blade.php
+++ resources/views/faculty-info/tutorials/index.blade.php
@@ -6,9 +6,7 @@
             <div class="card">
                 <div class="card-body">
                     <div class="row">
-                        <div class="col-4">
-                            <img src="/img/buX-logo.jpeg" class="img-fluid" alt="buX">
-                        </div>
+                        <div class="col-4 card-left-img" style="background-image: url('/img/buX-logo.jpeg');"></div>
                         <div class="col-8">
                             <h5 class="border-bottom"><a href="https://www.youtube.com/playlist?list=PL7v9IZwqVRs71c8tprHdfgvJxlsEEFWsa" target="_blank">Online Platform and Video Editing</a></h5>
                             <p>This includes all the tutorials about buX, video editing and other platforms that can be used to conduct online classes</p>
@@ -21,9 +19,7 @@
             <div class="card">
                 <div class="card-body">
                     <div class="row">
-                        <div class="col-4">
-                            <img src="https://www.bracu.ac.bd/sites/default/files/resources/media/bracu_logo.png" class="img-fluid" alt="Advising Student">
-                        </div>
+                        <div class="col-4 card-left-img" style="background-image: url('https://www.bracu.ac.bd/sites/default/files/resources/media/bracu_logo.png');"></div>
                         <div class="col-8">
                             <h5 class="border-bottom"><a href="https://docs.google.com/document/d/1iL74M_f51qpN_goszBxL6BUpaCYIUsMJyy_tDAHTNBs/view" target="_blank">Advising Students</a></h5>
                             <p>Learn how to use the USIS portal to advise students</p>
@@ -36,11 +32,9 @@
             <div class="card">
                 <div class="card-body">
                     <div class="row">
-                        <div class="col-4">
-                            <img src="https://ga1.imgix.net/logo/o/9460-1571335057-234728?auto=format&q=50&fit=fill" class="img-fluid" alt="Formstack">
-                        </div>
+                        <div class="col-4 card-left-img" style="background-image: url('https://ga1.imgix.net/logo/o/9460-1571335057-234728?auto=format&q=50&fit=fill');"></div>
                         <div class="col-8">
-                            <h5 class="border-bottom"><a href="https://www.youtube.com/playlist?list=PL7v9IZwqVRs71c8tprHdfgvJxlsEEFWsa" target="_blank">Formstack</a></h5>
+                            <h5 class="border-bottom"><a href="#/" target="_blank">Formstack</a></h5>
                             <p>Coming Soon...</p>
                         </div>
                     </div>
