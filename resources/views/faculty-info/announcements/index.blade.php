cd6f82211ae2b9578ab453af8f21442404deb622
Mobashir Monim <mobashirmonim@gmail.com>
2021-03-10 20:01:08 +0600

    Place announcement placeholder

--- resources/views/faculty-info/announcements/index.blade.php
+++ resources/views/faculty-info/announcements/index.blade.php
@@ -1,5 +1,9 @@
 @extends('faculty-info.layouts.app')
 
 @section('faculty-info.content')
-    Announcements
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <h4 class="text-center text-secondary border-bottom">No Announcements to show</h4>
+        </div>
+    </div>
 @endsection
--- resources/views/faculty-info/layouts/app.blade.php
+++ resources/views/faculty-info/layouts/app.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.dashboard')
 
 @section('content')
-    <nav class="navbar navbar-expand-lg navbar-light bg-light border-bottom" style="z-index: -1">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light border-bottom" style="z-index: 0">
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
         </button>
