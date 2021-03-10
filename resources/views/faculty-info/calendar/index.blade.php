1ace5a04227375e87a1f7a70d85aa89da51fdb50
Mobashir Monim <mobashirmonim@gmail.com>
2021-03-10 21:43:12 +0600

    Complete static calendar page

--- resources/views/faculty-info/calendar/index.blade.php
+++ resources/views/faculty-info/calendar/index.blade.php
@@ -1,5 +1,9 @@
 @extends('faculty-info.layouts.app')
 
 @section('faculty-info.content')
-    Calendar
+    <div class="row">
+        <div class="col-md-12 mb-3">
+            <iframe src="https://www.bracu.ac.bd/sites/default/files/registrar/Academic%20%20Planner%202021.pdf" frameborder="0" height="600px" width="100%"></iframe>
+        </div>
+    </div>
 @endsection
--- resources/views/faculty-info/tutorials/index.blade.php
+++ resources/views/faculty-info/tutorials/index.blade.php
@@ -1,5 +1,51 @@
 @extends('faculty-info.layouts.app')
 
 @section('faculty-info.content')
-    Tutorials
+    <div class="row">
+        <div class="col-md-6 my-3">
+            <div class="card">
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col-4">
+                            <img src="/img/buX-logo.jpeg" class="img-fluid" alt="buX">
+                        </div>
+                        <div class="col-8">
+                            <h5 class="border-bottom"><a href="https://www.youtube.com/playlist?list=PL7v9IZwqVRs71c8tprHdfgvJxlsEEFWsa" target="_blank">Online Platform and Video Editing</a></h5>
+                            <p>This includes all the tutorials about buX, video editing and other platforms that can be used to conduct online classes</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-6 my-3">
+            <div class="card">
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col-4">
+                            <img src="https://www.bracu.ac.bd/sites/default/files/resources/media/bracu_logo.png" class="img-fluid" alt="Advising Student">
+                        </div>
+                        <div class="col-8">
+                            <h5 class="border-bottom"><a href="https://docs.google.com/document/d/1iL74M_f51qpN_goszBxL6BUpaCYIUsMJyy_tDAHTNBs/view" target="_blank">Advising Students</a></h5>
+                            <p>Learn how to use the USIS portal to advise students</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-6 my-3">
+            <div class="card">
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col-4">
+                            <img src="https://ga1.imgix.net/logo/o/9460-1571335057-234728?auto=format&q=50&fit=fill" class="img-fluid" alt="Formstack">
+                        </div>
+                        <div class="col-8">
+                            <h5 class="border-bottom"><a href="https://www.youtube.com/playlist?list=PL7v9IZwqVRs71c8tprHdfgvJxlsEEFWsa" target="_blank">Formstack</a></h5>
+                            <p>Coming Soon...</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
