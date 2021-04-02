1ab80f1813d8b27c336b33910b9a37f05ded677f
Mobashir Monim <mobashirmonim@gmail.com>
2021-04-02 22:34:43 +0600

    Fix eval config button issue

--- resources/views/course-eval/index.blade.php
+++ resources/views/course-eval/index.blade.php
@@ -53,9 +53,9 @@
                                     @if (auth()->user()->email == 'mobashir.monim@bracu.ac.bd' && isset($helper))
                                         <div class="col-md-12 my-2 mt-4">
                                             <h5 class="border-bottom text-left">Evaluation Configurations</h5>
-                                            <button onclick="configFactors()" class="btn btn-dark d-inline-block m-1">Factors</button>
-                                            <button onclick="configMatrix()" class="btn btn-dark d-inline-block m-1">Matrix</button>
-                                            <button onclick="evaluateCourses()" class="btn btn-dark d-inline-block m-1">Evaluate</button>
+                                            <button onclick="configFactors()" type="button" class="btn btn-dark d-inline-block m-1">Factors</button>
+                                            <button onclick="configMatrix()" type="button" class="btn btn-dark d-inline-block m-1">Matrix</button>
+                                            <button onclick="evaluateCourses()" type="button" class="btn btn-dark d-inline-block m-1">Evaluate</button>
                                             @if ($helper->isPublishable())
                                                 <form action="{{ route('eval-report.publish-toggle', ['year' => $helper->year, 'semester' => $helper->semester]) }}" method="POST" class="d-inline">
                                                     @csrf
