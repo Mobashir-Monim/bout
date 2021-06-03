426e0b06198b276cd0885f6cb0caef2402c7da11
Mobashir Monim <mobashirmonim@gmail.com>
2021-06-03 22:36:14 +0600

    Remove placeholder values

--- resources/views/emailer/eval/index.blade.php
+++ resources/views/emailer/eval/index.blade.php
@@ -24,36 +24,36 @@
                                     Form URL:
                                 </div>
                                 <div class="col-8 my-2">
-                                    <input type="text" name="form_url" id="form_url" class="form-control" placeholder="Form URL" value="https://docs.google.com/forms/d/e/1FAIpQLSfr_YSNG3Bio_ahZncIaO8g4JsDYzP6HyPzG2cSUp9lH7bbFQ/viewform">
+                                    <input type="text" name="form_url" id="form_url" class="form-control" placeholder="Form URL">
                                 </div>
                                 <div class="col-4 my-auto">
                                     Course Code Key:
                                 </div>
                                 <div class="col-8 my-2">
-                                    <input type="text" name="cc_key" id="cc_key" class="form-control" placeholder="Course Code Key" value="entry.55736331">
+                                    <input type="text" name="cc_key" id="cc_key" class="form-control" placeholder="Course Code Key">
                                 </div>
                                 <div class="col-4 my-auto">
                                     Theory Section Key:
                                 </div>
                                 <div class="col-8 my-2">
-                                    <input type="text" name="ts_key" id="ts_key" class="form-control" placeholder="Theory Section Key" value="entry.2049915700">
+                                    <input type="text" name="ts_key" id="ts_key" class="form-control" placeholder="Theory Section Key">
                                 </div>
                                 <div class="col-4 my-auto">
                                     Lab Section Key (1):
                                 </div>
                                 <div class="col-8 my-2">
-                                    <input type="text" name="ls1_key" id="ls1_key" class="form-control" placeholder="Lab Section Key" value="entry.495890793">
+                                    <input type="text" name="ls1_key" id="ls1_key" class="form-control" placeholder="Lab Section Key">
                                 </div>
                                 <div class="col-4 my-auto">
                                     Lab Section Key (2):
                                 </div>
                                 <div class="col-8 my-2">
-                                    <input type="text" name="ls2_key" id="ls2_key" class="form-control" placeholder="Lab Section Key" value="entry.375660266">
+                                    <input type="text" name="ls2_key" id="ls2_key" class="form-control" placeholder="Lab Section Key">
                                 </div>
                             </div>
                         </div>
                         <div class="col-md-6 my-2">
-                            <textarea name="lab_courses" id="lab_courses" class="form-control" cols="30" rows="12" placeholder="Lab Courses (comma separated)">CSE101, CSE103, CSE110, CSE111, CSE220, CSE221, , CSE250, CSE251, CSE260, CSE310, CSE321, CSE330, CSE341, CSE350, CSE360, CSE370, CSE391, CSE419, CSE420, CSE421, CSE422, CSE423, CSE430, CSE460, CSE461, CSE471, CSE474</textarea>
+                            <textarea name="lab_courses" id="lab_courses" class="form-control" cols="30" rows="12" placeholder="Lab Courses (comma separated)"></textarea>
                         </div>
                         <div class="col-md-6 my-2" id="email-stats">
 
