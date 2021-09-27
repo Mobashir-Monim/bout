0a0af51041189bdaecd1357d4fe741f447750d8e
Mobashir Monim <mobashirmonim@gmail.com>
2021-09-27 23:40:57 +0600

    Fix typo

--- resources/views/emailer/eval/index.blade.php
+++ resources/views/emailer/eval/index.blade.php
@@ -8,7 +8,7 @@
                     <h5 class="border-bottom">Evaluation Emailer</h5>
                     <div class="row">
                         <div class="col-md-12 my-2">
-                            <input type="text" name="subject" id="subject" class="form-control" placeholder="Email Subject" value="Course Evlauation">
+                            <input type="text" name="subject" id="subject" class="form-control" placeholder="Email Subject" value="Course Evaluation">
                         </div>
                     </div>
                     <div class="row">
