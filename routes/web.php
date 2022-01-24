82f315e0d3342caf35053eb7681f2135577633e2
Mobashir Monim <mobashirmonim@gmail.com>
2022-01-24 20:08:08 +0600

    debug

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 Route::get('test', function () {
-    phpinfo();
     $x = App\Models\StudentMap::where('email', 'like', '%,%')->get();
     dd($x->toArray());
     foreach ($x as $y) {
