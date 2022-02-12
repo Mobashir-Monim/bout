e73e4d4f564cfb6d4f568191f9ee06f9b6da6017
Mobashir Monim <mobashirmonim@gmail.com>
2022-02-12 22:29:28 +0600

    debug

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 Route::get('test', function () {
-    $ocss = App\Models\OfferedCourseSection::all();
+    $ocss = App\Models\OfferedCourse::all();
 
     foreach ($ocss as $ocs) {
         try {
